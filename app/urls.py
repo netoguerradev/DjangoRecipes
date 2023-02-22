54d486b815ac7b09122ee366c53436254330148b
netooguerradev <valterguerradev@hotmail.com>
2023-02-22 19:41:41 -0300

    add models

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,6 @@ from . import views
 # Informa o caminho da url, subdominio e a função que irá ser executada quando for feito a requisição da rota
 
 urlpatterns = [
-    path('', views.home),
-    path('recipe/<int:id>/', views.recipe)
+    path('', views.home, name="recipe-home"),
+    path('recipe/<int:id>/', views.recipe, name="recipes-recipe")
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 #Define as funções que indicam o que será feito naquela rota e envia para o urls
 
 def home(request):
-    return render(request, 'global/home.html')
+    return render(request, 'global/home.html', context={
+        'recipes': [make_recipe() for _ in range(3)]
+    })
 
 
-def recipe(request,id):
-    return render(request, 'global/recipe-view.html')
+def recipe(request, id):
+    return render(request, 'global/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True
+    })
