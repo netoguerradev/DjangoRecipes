2f79818ae163839d6cbba2c279b82a585e29e0a8
netooguerradev <valterguerradev@hotmail.com>
2023-02-27 08:07:26 -0300

    add models

--- app/models.py
+++ app/models.py
@@ -1,12 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
 
 class Recipe(models.Model):
     title: models.CharField(max_length=65)
     description: models.CharField(max_length=165)
-    slug: models.models.SlugField()
-    preparation_time: models.integerField()
+    slug: models.SlugField()
+    preparation_time: models.IntegerField()
     preparation_time_unit: models.CharField( max_length=50)
-    servings = models.integerField()
+    servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
     preparation_steps_is_html = models.BooleanField(default=False)
@@ -14,3 +19,5 @@ class Recipe(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     is_Published = models.BooleanField(default=False)
     cover = models.ImageField(upload_to='recipes/cover/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
